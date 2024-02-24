bb350286a1b3f93cdfb08874d11da304c5599150
shiv1810 <shivmistry978@gmail.com>
2024-02-24 23:21:37 +0530

    Working Home Page

---
+++ SPPA/AttendanceView.swift
@@ -0,0 +1,18 @@
+//
+//  AttendanceView.swift
+//  SPPA
+//
+//  Created by Shiv Mistry on 24/02/24.
+//
+
+import SwiftUI
+
+struct AttendanceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AttendanceView()
+}
---
+++ SPPA/Student Dashboard/TimeTable View.swift
@@ -0,0 +1,18 @@
+//
+//  TimeTable View.swift
+//  SPPA
+//
+//  Created by Shiv Mistry on 24/02/24.
+//
+
+import SwiftUI
+
+struct TimeTable_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimeTable_View()
+}
